042ce6c8bec07b3b1b4133d5b34b42fd8c3139c4
Sam Park <samuel.culver.park@gmail.com>
2022-06-09 14:31:23 -0400

    setting up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+// import { createRoot } from 'react-dom/client';
+// import App from './App';
+// import './index.css';
+
+// const container = document.getElementById('root');
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(<App tab="home" />);
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +20,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
