67fe0c7b96771eb10b3b391aa01ec182ac1c00d1
Sam Park <samuel.culver.park@gmail.com>
2022-06-13 16:40:52 -0400

    initial file stucture

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'
+import { registerLicense } from '@syncfusion/ej2-base';
+
+registerLicense(`ORg4AjUWIQA/Gnt2VVhhQlFaclhJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRdkNiWn5YcnVQRGdYVkw=`);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
